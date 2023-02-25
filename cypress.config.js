7ca3e5ec1721589fa87d844f233f823f7a6b2236
Siddeshwar Mali <VDPT0775@finnovation.org>
2023-02-25 18:48:43 +0530

    Added package.json

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'ksi95u',
+  videoCompression:false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      
+    },
+  },
+});
