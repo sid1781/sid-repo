735509c407554fb52431f942e025dacfcd9cffc2
Siddeshwar Mali <VDPT0775@finnovation.org>
2023-02-25 19:15:52 +0530

    removed record key from config.yml

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'ksi95u',
   videoCompression:false,
+  CYPRESS_RECORD_KEY: "cf1c80c6-4337-49d4-ba37-fd916507d14e",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
