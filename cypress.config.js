c8d3e216f676647263c49f57aac37d7c05bbf8d4
Siddeshwar Mali <VDPT0775@finnovation.org>
2023-03-20 11:11:12 +0530

    HTML Report Generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter : 'cypress-mochawesome-reporter',
   projectId: 'r5abjh',
   videoCompression:false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-multi-reporters/plugin')(on)
       
     },
   },
