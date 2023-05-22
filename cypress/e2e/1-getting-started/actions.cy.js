5d7044615fe82625054aa0717d200aa0e3a05a0a
Siddeshwar Mali <VDPT0775@finnovation.org>
2023-05-22 15:23:11 +0530

    Pushed for Trial Run

--- cypress/e2e/1-getting-started/actions.cy.js
+++ cypress/e2e/1-getting-started/actions.cy.js
@@ -15,14 +15,10 @@ context('Actions', () => {
       // .type() with special character sequences
       .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
       .type('{del}{selectall}{backspace}')
-
-      // .type() with key modifiers
       .type('{alt}{option}') //these are equivalent
       .type('{ctrl}{control}') //these are equivalent
       .type('{meta}{command}{cmd}') //these are equivalent
       .type('{shift}')
-
-      // Delay each keypress by 0.1 sec
       .type('slow.typing@email.com', { delay: 100 })
       .should('have.value', 'slow.typing@email.com')
 
