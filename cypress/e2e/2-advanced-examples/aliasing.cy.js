b19d8f5bfcb1c676331191358fb5ab7cf20e9ebf
Siddeshwar Mali <VDPT0775@finnovation.org>
2023-05-22 15:33:13 +0530

    Pushed for Trial Run1 from CircleCi Branch

--- cypress/e2e/2-advanced-examples/aliasing.cy.js
+++ cypress/e2e/2-advanced-examples/aliasing.cy.js
@@ -6,11 +6,7 @@ context('Aliasing', () => {
   })
 
   it('.as() - alias a DOM element for later use', () => {
-    // https://on.cypress.io/as
-
-    // Alias a DOM element for use later
-    // We don't have to traverse to the element
-    // later in our code, we reference it with @
+    
 
     cy.get('.as-table').find('tbody>tr')
       .first().find('td').first()
